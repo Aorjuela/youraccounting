29b8eb538166a103cc7326ae97bfe6675a68e730
ingjairoorjuela <jairoorjuela@ssintco.com>
2021-04-02 21:10:43 -0500

    añadir clase EgressDatabase

---
+++ app/src/main/java/com/example/youraccounting/EgressDatabase.kt
@@ -0,0 +1,31 @@
+package com.example.prueba
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Egress::class], version = 3, exportSchema = false)
+abstract class EgressDatabase : RoomDatabase() {
+
+    abstract fun egressDao(): EgressDao
+
+    companion object {
+        var INSTANCE: EgressDatabase? = null
+
+        fun getInstance(context: Context): EgressDatabase? {
+            if (INSTANCE == null){
+                synchronized(EgressDatabase::class){
+                    INSTANCE = Room.databaseBuilder(context.applicationContext, EgressDatabase::class.java, "myDBEgresses")
+                        .fallbackToDestructiveMigration()
+                        .build()
+                }
+            }
+            return INSTANCE
+        }
+
+        fun destroyDataBase(){
+            INSTANCE = null
+        }
+    }
+}
